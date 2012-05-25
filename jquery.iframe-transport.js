07acb00377ce0e4acee693c54425102f57eb740c
Pau <masylum@gmail.com>
2012-05-25 17:12:15 +0200

    jslint the hell out of it

--- jquery.iframe-transport.js
+++ jquery.iframe-transport.js
@@ -70,7 +70,7 @@
 // impossible for the javascript code to determine the HTTP status code of the
 // servers response. Effectively, all of the calls you make will look like they
 // are getting successful responses, and thus invoke the `done()` or
-// `complete()` callbacks. You can only determine communicate problems using
+// `complete()` callbacks. You can only determine communicate problems using
 // the content of the response payload. For example, consider using a JSON
 // response such as the following to indicate a problem with an uploaded file:
 
@@ -89,12 +89,12 @@
 
 // ## Annotated Source
 
-(function($, undefined) {
+(function ($, undefined) {
   "use strict";
 
   // Register a prefilter that checks whether the `iframe` option is set, and
   // switches to the "iframe" data type if it is `true`.
-  $.ajaxPrefilter(function(options, origOptions, jqXHR) {
+  $.ajaxPrefilter(function (options, origOptions, jqXHR) {
     if (options.iframe) {
       return "iframe";
     }
@@ -103,7 +103,7 @@
   // Register a transport for the "iframe" data type. It will only activate
   // when the "files" option has been set to a non-empty list of enabled file
   // inputs.
-  $.ajaxTransport("iframe", function(options, origOptions, jqXHR) {
+  $.ajaxTransport("iframe", function (options, origOptions, jqXHR) {
     var form = null,
         iframe = null,
         name = "iframe-" + $.now(),
@@ -114,7 +114,7 @@
     // and should revert all changes made to the page to enable the
     // submission via this transport.
     function cleanUp() {
-      markers.replaceWith(function(idx) {
+      markers.replaceWith(function (idx) {
         return files.get(idx);
       });
       form.remove();
@@ -127,8 +127,8 @@
     options.dataTypes.shift();
 
     if (files.length) {
-      form = $("<form enctype='multipart/form-data' method='post'></form>").
-        hide().attr({action: options.url, target: name});
+      form = $("<form enctype='multipart/form-data' method='post'></form>")
+        .hide().attr({action: options.url, target: name});
 
       // If there is any additional data specified via the `data` option,
       // we add it as hidden fields to the form. This (currently) requires
@@ -149,14 +149,14 @@
       // Add a hidden `X-Requested-With` field with the value `IFrame` to the
       // field, to help server-side code to determine that the upload happened
       // through this transport.
-      $("<input type='hidden' value='IFrame' name='X-Requested-With' />").
-        appendTo(form);
+      $("<input type='hidden' value='IFrame' name='X-Requested-With' />")
+        .appendTo(form);
 
       // Move the file fields into the hidden form, but first remember their
       // original locations in the document by replacing them with disabled
       // clones. This should also avoid introducing unwanted changes to the
       // page layout during submission.
-      markers = files.after(function(idx) {
+      markers = files.after(function (idx) {
         return $(this).clone().prop("disabled", true);
       }).next();
       files.appendTo(form);
@@ -165,19 +165,19 @@
 
         // The `send` function is called by jQuery when the request should be
         // sent.
-        send: function(headers, completeCallback) {
+        send: function (headers, completeCallback) {
           iframe = $("<iframe src='javascript:false;' name='" + name +
             "' id='" + name + "' style='display:none'></iframe>");
 
           // The first load event gets fired after the iframe has been injected
           // into the DOM, and is used to prepare the actual submission.
-          iframe.bind("load", function() {
+          iframe.bind("load", function () {
 
             // The second load event gets fired when the response to the form
             // submission is received. The implementation detects whether the
             // actual payload is embedded in a `<textarea>` element, and
             // prepares the required conversions to be made in that case.
-            iframe.unbind("load").bind("load", function() {
+            iframe.unbind("load").bind("load", function () {
               var doc = this.contentWindow ? this.contentWindow.document :
                 (this.contentDocument ? this.contentDocument : this.document),
                 root = doc.documentElement ? doc.documentElement : doc.body,
@@ -209,15 +209,14 @@
 
         // The `abort` function is called by jQuery when the request should be
         // aborted.
-        abort: function() {
+        abort: function () {
           if (iframe !== null) {
             iframe.unbind("load").attr("src", "javascript:false;");
             cleanUp();
           }
         }
-
       };
     }
   });
 
-})(jQuery);
+}(jQuery));
